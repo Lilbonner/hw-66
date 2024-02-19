472ebeeae1b14e9959d9249b96347e3dffc13f5e
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-20 01:19:56 +0600

    created a navigation bar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+    return (
+        <div className="border-b-2 border-black text-2xl bg-blue-400 h-12">
+            <h2 className="ml-10 pt-2">Calorie tracker</h2>
+        </div>
+    );
+};
+
+export default Navbar;
